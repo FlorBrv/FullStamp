47a9a60746bbbdebd518d4f85280a8a6758127ed
Florencia Bravo <flor.bravo.98@gmail.com>
2023-09-17 23:11:41 -0300

    Estructura base del sitio web

---
+++ main.js
@@ -0,0 +1,51 @@
+function mostrarPrecios(){
+    var medidas = document.getElementById("medidas");
+    console.log(medidas.value);
+    if ("1"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$750"
+    }
+    
+    if ("2"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$800 (Personalizado $950)"
+    }
+
+    if ("3"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$1350"
+    }
+    if ("4"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$1400"
+    }
+    if ("5"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$1500"
+    }
+    if ("6"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$1800"
+    }
+    if ("7"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$2100"
+    }
+    if ("8"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$2580"
+    }
+    if ("9"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$2900"
+    }
+    if ("10"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$3300"
+    }
+    if ("11"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$3900"
+    }
+
+    if ("12"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$3500"
+    }
+    if ("13"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$3700"
+    }
+  
+    
+    }
+
+   
+    
