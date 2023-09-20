ed18d618e6bda9885a9a650dd9a9fe0eb2d6a4ff
Florencia Brv <137433933+FlorBrv@users.noreply.github.com>
2023-09-20 13:31:05 -0300

    Update main.js
    Precios actualizados sept

--- main.js
+++ main.js
@@ -2,46 +2,46 @@ function mostrarPrecios(){
     var medidas = document.getElementById("medidas");
     console.log(medidas.value);
     if ("1"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$750"
+        document.getElementById("div_precios").innerHTML="$1200"
     }
     
     if ("2"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$800 (Personalizado $950)"
+        document.getElementById("div_precios").innerHTML="$1300 (Personalizado $1500)"
     }
 
     if ("3"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$1350"
+        document.getElementById("div_precios").innerHTML="$1580"
     }
     if ("4"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$1400"
+        document.getElementById("div_precios").innerHTML="$1680"
     }
     if ("5"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$1500"
-    }
-    if ("6"==medidas.value){
         document.getElementById("div_precios").innerHTML="$1800"
     }
+    if ("6"==medidas.value){
+        document.getElementById("div_precios").innerHTML="$1980"
+    }
     if ("7"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$2100"
+        document.getElementById("div_precios").innerHTML="$2280"
     }
     if ("8"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$2580"
+        document.getElementById("div_precios").innerHTML="$2680"
     }
     if ("9"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$2900"
+        document.getElementById("div_precios").innerHTML="$2990"
     }
     if ("10"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$3300"
+        document.getElementById("div_precios").innerHTML="$3590"
     }
     if ("11"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$3900"
+        document.getElementById("div_precios").innerHTML="$4500"
     }
 
     if ("12"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$3500"
+        document.getElementById("div_precios").innerHTML="$3800"
     }
     if ("13"==medidas.value){
-        document.getElementById("div_precios").innerHTML="$3700"
+        document.getElementById("div_precios").innerHTML="$3900"
     }
   
     
